d59b61ac3e5180f592996862bce77a2bb552887e
Jaime D <134320299+jdoerr13@users.noreply.github.com>
2025-05-30 12:43:24 -0400

    import React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import React from "react";  
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Nunito } from "next/font/google";
